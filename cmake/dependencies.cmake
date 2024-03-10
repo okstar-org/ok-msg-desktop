65fa446522d7ca6558efef83fffc9e35f8e201f2
cto <cto@chuanshaninfo.com>
2024-03-10 17:33:58 +0800

    cmake: 配置Linux环境兼容

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -5,28 +5,7 @@ if(WIN32)
         message(FATAL_ERROR "请在环境变量配置Qt路径【QTDIR】！")
     endif()
     message(STATUS "QT_DIR=${QT_DIR}")
-
     set(CMAKE_PREFIX_PATH ${QT_DIR})
-
-    # Qt
-    set(CMAKE_AUTOMOC ON)
-    set(CMAKE_AUTOUIC ON)
-    set(CMAKE_AUTORCC ON)
-
-    find_package(Qt5 COMPONENTS Core
-            Concurrent
-            Widgets
-            Gui
-            Multimedia
-            MultimediaWidgets
-            Network
-            Xml
-            Sql
-            Svg
-            OpenGL
-            LinguistTools
-            UiTools
-            REQUIRED)
 
     # 设置Qt模块包含头文件和库
     include_directories(${CMAKE_PREFIX_PATH}/include)
@@ -41,6 +20,26 @@ if(WIN32)
     message(STATUS "LINK_STATIC_QT=${LINK_STATIC_QT}")
 endif()
 
+# Qt
+set(CMAKE_AUTOMOC ON)
+set(CMAKE_AUTOUIC ON)
+set(CMAKE_AUTORCC ON)
+
+find_package(Qt5 COMPONENTS Core
+        Concurrent
+        Widgets
+        Gui
+        Multimedia
+        MultimediaWidgets
+        Network
+        Xml
+        Sql
+        Svg
+        OpenGL
+        LinguistTools
+        UiTools
+        REQUIRED)
+
 if(UNIX)
     include_directories(${Qt5LinuxAccessibilitySupport_INCLUDES})
     set(Qt5LinuxAccessibilitySupport_INCLUDES
@@ -48,4 +47,4 @@ if(UNIX)
 endif()
 
 # 开启插件（ON/OFF）
-set(ENABLE_PLUGINS OFF)
+option(ENABLE_PLUGINS "Enable plugins" ON)
