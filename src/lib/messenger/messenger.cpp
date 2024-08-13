73397dabad36bfeeb13bcf6e7ffaa61ef82b315c
Joy1024 <412555203@qq.com>
2024-08-13 19:47:24 +0800

    Fix for cmake

--- src/lib/messenger/messenger.cpp
+++ src/lib/messenger/messenger.cpp
@@ -15,9 +15,9 @@
 #include <QThread>
 #include <memory>
 
+#include "application.h"
 #include "base/logs.h"
 #include "base/xmls.h"
-#include "src/application.h"
 
 #include "lib/messenger/IM.h"
 #include "lib/messenger/IMConference.h"
