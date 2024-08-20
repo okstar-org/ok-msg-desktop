fec96a3f9ab78cd32b13135f5b31d14292c44b24
Joy1024 <412555203@qq.com>
2024-08-20 17:07:03 +0800

    优化js代码

--- src/modules/platform/res/html/platform.js
+++ src/modules/platform/res/html/platform.js
@@ -1,13 +1,13 @@
-/**
- *   ~ Copyright (c) 2022 船山信息 chuanshaninfo.com
- *   ~ The project is licensed under Mulan PubL v2.
- *   ~ You can use this software according to the terms and conditions of the Mulan
- *   ~ PubL v2. You may obtain a copy of Mulan PubL v2 at:
- *   ~          http://license.coscl.org.cn/MulanPubL-2.0
- *   ~ THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
- *   ~ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
- *   ~ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- *   ~ See the Mulan PubL v2 for more details.
+/*
+ * Copyright (c) 2022 船山信息 chuanshaninfo.com
+ * The project is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ * PubL v2. You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
  */
 (function () {
     
@@ -18,11 +18,11 @@
     var socket = new WebSocket(wsUri);
     function appItemClicked(event) {
         let app = event.data;
-        let homePage = (app.homePage instanceof String) ? encodeURI(app.homePage) : "";
+        let {name, homePage} = app;
         let data = {
             command: "app-center.openApp",
             homePage,
-            name: app.name
+            name
         }
         socket.send(JSON.stringify(data));
     }
