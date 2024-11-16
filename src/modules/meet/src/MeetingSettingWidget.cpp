006eac89f2d791f427ca047eba9437117b356669
liutao <398741905@qq.com>
2024-11-16 13:42:55 +0800

    完成会议设置界面

--- src/modules/meet/src/MeetingSettingWidget.cpp
+++ src/modules/meet/src/MeetingSettingWidget.cpp
@@ -1,3 +1,19 @@
 #include "MeetingSettingWidget.h"
+#include "ui_MeetingSettingWidget.h"
+#include <base/shadowbackground.h>
 
-MeetingSettingWidget::MeetingSettingWidget(QWidget* parent) : QWidget(parent){}
+MeetingSettingWidget::MeetingSettingWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::MeetingSettingWidget)
+{
+    setContentsMargins(10, 10, 10, 10);
+    ShadowBackground* shadowBack = new ShadowBackground(this);
+    shadowBack->setShadowRadius(10);
+
+    ui->setupUi(this);
+}
+
+MeetingSettingWidget::~MeetingSettingWidget()
+{
+    delete ui;
+}
--- src/modules/meet/src/MeetingSettingWidget.h
+++ src/modules/meet/src/MeetingSettingWidget.h
@@ -1,20 +1,22 @@
-#pragma once
-/*
- * Copyright (c) 2022 船山信息 chuanshaninfo.com
- * The project is licensed under Mulan PubL v2.
- * You can use this software according to the terms and conditions of the Mulan
- * PubL v2. You may obtain a copy of Mulan PubL v2 at:
- *          http://license.coscl.org.cn/MulanPubL-2.0
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
- * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
- * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- * See the Mulan PubL v2 for more details.
- */
+#ifndef MEETINGSETTINGWIDGET_H
+#define MEETINGSETTINGWIDGET_H
 
 #include <QWidget>
 
-class MeetingSettingWidget : public QWidget {
+namespace Ui {
+class MeetingSettingWidget;
+}
+
+class MeetingSettingWidget : public QWidget
+{
     Q_OBJECT
+
 public:
-    MeetingSettingWidget(QWidget* parent = nullptr);
+    explicit MeetingSettingWidget(QWidget *parent = nullptr);
+    ~MeetingSettingWidget();
+
+private:
+    Ui::MeetingSettingWidget *ui;
 };
+
+#endif // MEETINGSETTINGWIDGET_H
